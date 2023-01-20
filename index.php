f6c96e079f9b3fefbfdbe18aea56b95ee1eadd15
imkarthi16 <karthiveera04@gmail.com>
2023-01-20 15:51:27 +0530

    PHP simple e-comm app | Working with mysql database connection

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
+<?php 
+// connect to databse
+$conn = mysqli_connect('localhost','karthick','karthick@2023','ecommerce');
 
+// check connection
+if (!$conn) {
+    echo 'Database connection failed: ' . mysqli_connect_error();
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
