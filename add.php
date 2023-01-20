07bcb1778504d2ee8389f744367094ee8ab3ecc3
imkarthi16 <karthiveera04@gmail.com>
2023-01-20 12:00:06 +0530

    PHP simple e-comm app | Working with update form & showing errors

--- add.php
+++ add.php
@@ -1,66 +1,67 @@
 <?php 
 
+	$email = $title = $ingredients = '';
+	$errors = array('email' => '', 'title' => '', 'ingredients' => '');
 
-if (isset($_POST['submit'])) {
-    
-    // check email
-	if(empty($_POST['email'])){
-		echo 'An email is required <br />';
-	} else{
-		$email = $_POST['email'];
-		if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-			echo 'Email must be a valid email address';
+	if(isset($_POST['submit'])){
+		
+		// check email
+		if(empty($_POST['email'])){
+			$errors['email'] = 'An email is required';
+		} else{
+			$email = $_POST['email'];
+			if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+				$errors['email'] = 'Email must be a valid email address';
+			}
 		}
-	}
 
-    // check title
-	if(empty($_POST['title'])){
-		echo 'A title is required <br />';
-	} else{
-		$title = $_POST['title'];
-		if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
-				echo 'Title must be letters and spaces only';
+		// check title
+		if(empty($_POST['title'])){
+			$errors['title'] = 'A title is required';
+		} else{
+			$title = $_POST['title'];
+			if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+				$errors['title'] = 'Title must be letters and spaces only';
+			}
 		}
-	}
 
-    // check ingredients
-	if(empty($_POST['ingredients'])){
-		echo 'At least one ingredient is required <br />';
-	} else{
-		$ingredients = $_POST['ingredients'];
-		if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
-			echo 'Ingredients must be a comma separated list';
+		// check ingredients
+		if(empty($_POST['ingredients'])){
+			$errors['ingredients'] = 'At least one ingredient is required';
+		} else{
+			$ingredients = $_POST['ingredients'];
+			if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+				$errors['ingredients'] = 'Ingredients must be a comma separated list';
+			}
 		}
-	}
 
 	} // end POST check
 
 ?>
 
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
+	
+	<?php include('templates/header.php'); ?>
 
-<?php require ('templates/header.php');?>
+	<section class="container grey-text">
+		<h4 class="center">Add a Pizza</h4>
+		<form class="white" action="add.php" method="POST">
+			<label>Your Email</label>
+			<input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
+			<div class="red-text"><?php echo $errors['email']; ?></div>
+			<label>Pizza Title</label>
+			<input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
+			<div class="red-text"><?php echo $errors['title']; ?></div>
+			<label>Ingredients (comma separated)</label>
+			<input type="text" name="ingredients" value="<?php echo htmlspecialchars($ingredients) ?>">
+			<div class="red-text"><?php echo $errors['ingredients']; ?></div>
+			<div class="center">
+				<input type="submit" name="submit" value="Submit" class="btn brand z-depth-0">
+			</div>
+		</form>
+	</section>
 
-<section class="container grey-text">
-    <h4 class="center">Add Pizza</h4>
-    <form class="white" action="add.php" method="POST">
-        <label>Your Email:</label>
-        <input type="email" name="email">
-        <label>Pizza Title:</label>
-        <input type="text" name="title">
-        <label>Ingredients:(comma seprated)</label>
-        <input type="text" name="ingredients">
-        <br><br>
-        <div class="center">
-             <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
-        </div>
-    </form>
-</section>
-
-
-
-<?php require ('templates/footer.php');?>
+	<?php include('templates/footer.php'); ?>
 
 </html>
