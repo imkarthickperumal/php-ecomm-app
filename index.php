5a418a6a48c4b0caa384e5908c4077acce3acfbd
imkarthi16 <karthiveera04@gmail.com>
2023-01-21 09:51:55 +0530

    PHP simple e-comm app | Working with mysql getting data from database

--- index.php
+++ index.php
@@ -6,6 +6,24 @@ $conn = mysqli_connect('localhost','karthick','karthick@2023','ecommerce');
 if (!$conn) {
     echo 'Database connection failed: ' . mysqli_connect_error();
 }
+
+// write a query for all pizzas
+$sql = 'SELECT title, ingredients, id FROM pizza';
+
+// make a query & get result
+$result = mysqli_query($conn, $sql);
+
+// fetch the resulting rows as an array
+$pizza = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// free result memory
+mysqli_free_result($result);
+
+// close conncetion
+mysqli_close($conn);
+
+print_r($pizza);
+
 ?>
 
 <!DOCTYPE html>
