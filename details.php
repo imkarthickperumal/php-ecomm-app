c7fa0af44fbb565868af68ba5e9652e97b84ca3d
imkarthi16 <karthiveera04@gmail.com>
2023-01-21 15:23:44 +0530

    PHP simple e-comm app | Working with deleting a record

--- details.php
+++ details.php
@@ -1,6 +1,21 @@
 <?php 
 
   include('config/db_connect.php');
+
+  // delete pizza details
+  if(isset($_POST['delete'])){
+    $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+
+    $sql = "DELETE FROM pizza WHERE id = $id_to_delete";
+
+    if(mysqli_query($conn,$sql)){
+        // success
+        header('Location: index.php');
+    } else{
+        // failure
+        echo 'query error:' . mysqli_error($conn);
+    }
+  }
   
   // check GET request id param
   if(isset($_GET['id'])){
@@ -35,6 +50,13 @@
                 <p><?php echo date($pizzas['created_at']);?></p>
                 <h5>Ingredients:</h5>
                 <p><?php echo htmlspecialchars($pizzas['ingredients']);?></p>
+
+
+                <!-- DELETE FORM -->
+                <form action="details.php" method="POST">
+                    <input type="hidden" name="id_to_delete" value="<?php echo $pizzas['id']?>">
+                    <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+                </form>
             <?php else:?>
                 <h5>No Pizza Exists!</h5>
             <?php endif;?>
